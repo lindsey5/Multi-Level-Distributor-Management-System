72034f25b841485c18a3379c9f464de87bfd72d4
lindseysamson5 <lindseysamson5@gmail.com>
2026-04-11 02:42:31 +0800

    commit

--- frontend/src/components/ui/DropdownMenu.tsx
+++ frontend/src/components/ui/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { LogOut, Menu, User } from "lucide-react";
 import type { MenuItem } from "../../types/menu.type";
